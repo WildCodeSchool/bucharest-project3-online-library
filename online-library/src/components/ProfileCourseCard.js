c538463ce648ccda3c4821482848361d2891f84b
Olivier Ggr <grandgerard.olivier@gmail.com>
2019-12-05 15:53:26 +0200

    First commit of the component for the course cards inside the profile

---
+++ online-library/src/components/ProfileCourseCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import '../App.css';
+
+class ProfileCourseCard extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            firstname : ''
+        }
+    }
+
+    render(){
+        return(
+            <div className="profileCourseCardMain">
+                <header>
+                    <h3>
+                        {this.props.chapterTitle}
+                    </h3>
+                </header>
+
+                <div className="profileCourseCardBody">
+                    <h4>                    
+                        {this.props.courseTitle}
+                    </h4>
+                    <button>
+                        <a href={this.props.courseLink}>Link to Course</a>
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ProfileCourseCard;

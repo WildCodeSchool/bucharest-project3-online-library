d74baedd84b9e22fbc8b026741d2f72ca4200aae
AndreeaO3 <oanandreeadin@gmail.com>
2019-12-09 17:35:48 +0200

    done

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Footer.scss';
+
+
+
+class Footer extends Component {
+
+    render() {
+        return (
+            <footer class="footer">
+                <p class="text"> @2018 Ajungem Mari. Toate drepturile rezervate.</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer

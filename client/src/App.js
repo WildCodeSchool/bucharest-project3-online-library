b7d2752821f8247424407a72d774730737551a2b
Olivier Ggr <grandgerard.olivier@gmail.com>
2019-12-10 14:48:31 +0200

    fitler structure and style done for desktop + mobile

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Filter from './components/filter/filter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Filter />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/filter/filter.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel'
+import Checkbox from '@material-ui/core/Checkbox';
+
+import './filter.css'
+
+class Filter extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            keyword: '',
+            title: '',
+            category: '',
+            important : false,
+            completed : false,
+            nonCompleted : false
+        }
+    }
+
+    handleChangeKeyword = (e) => {
+        this.setState({ keyword: e.target.value })
+    }
+
+    handleChangeTitle = (e) => {
+        this.setState({ title : e.target.value })
+    }
+
+    handleChangeCategory = (e) => {
+        this.setState({ category : e.target.value})
+    }
+
+    handleChangeImportant = (e) => {
+        this.setState({ important : !this.state.important})
+    }
+
+    handleChangeCompleted = (e) => {
+        this.setState({ completed : !this.state.completed})
+    }
+
+    handleChangeNonCompleted = (e) => {
+        this.setState({ nonCompleted : !this.state.nonCompleted})
+    }
+
+    render(){
+        return(
+            <div className="filterMain">
+                <form action="">
+                    <div className="row">
+                        <div className="filterTop">
+                        <TextField id="outlined-basic" label="Keywords" value={this.state.keyword} className="filterInput" variant="outlined" onChange={this.handleChangeKeyword}/>
+                        <TextField id="outlined-basic" label="Title" value={this.state.title} className="filterInput filterInputCenter" variant="outlined" onChange={this.handleChangeTitle}/>
+                        <FormControl className="filterInput">
+                            <InputLabel id="categoryLabel">Category</InputLabel>
+                            <Select
+                            labelId="demo-simple-select-autowidth-label"
+                            id="demo-simple-select-autowidth"
+                            value={this.state.category}
+                            onChange={this.handleChangeCategory}
+                            autoWidth
+                            >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={'Maths'}>Maths</MenuItem>
+                            <MenuItem value={'History'}>History</MenuItem>
+                            <MenuItem value={'Economics'}>Economics</MenuItem>
+                            </Select>
+                        </FormControl>
+
+
+
+
+
+
+
+                        </div>
+                        <div className="filterBottom">
+                        <FormControlLabel
+                            className="checkboxInput"
+                            control={
+                                <Checkbox checked={this.state.important} onChange={this.handleChangeImportant} value="Important" color="primary" />} label="Important" 
+                        />
+                        <FormControlLabel
+                                                    className="checkboxInput"
+
+                            control={
+                                <Checkbox checked={this.state.completed} onChange={this.handleChangeCompleted} value="Completed" color="primary" />} label="Completed" 
+                        />
+                        <FormControlLabel
+                                                    className="checkboxInput"
+
+                            control={
+                        <Checkbox checked={this.state.nonCompleted} onChange={this.handleChangeNonCompleted} value="Non-Completed" color="primary" />} label="Non-Completed" 
+                        />
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default Filter;

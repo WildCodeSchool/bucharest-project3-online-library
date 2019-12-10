c31bc1f187dfe79c3a2e52903f4d4bc727c8e697
Olivier Ggr <grandgerard.olivier@gmail.com>
2019-12-10 14:17:28 +0200

    signUp state management fix

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import SignUp from './components/SignUp/SignUp'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <SignUp />
+    </div>
+  );
+}
+
+export default App;

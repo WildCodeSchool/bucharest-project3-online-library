2be5b099c9495377262fec95bb07c137e47f080d
Antonia Dobrescu <antonia.dobrescu91@gmail.com>
2019-12-11 10:38:48 +0200

    added props to the cardComponent

--- src/App.js
+++ src/App.js
@@ -8,13 +8,20 @@ class App extends React.Component {
       headerColor: "#FA5457",
       chapterCard: 'Chapter Title',
       textCard: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue eros egestas aliquet viverra. Mauris feugiat ultrices odi variualiqua. Curabitur vestibulum, quam et dignissim porttitor,diam erat varius est Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue eros egestas aliquet viverra. Mauris feugiat ultrices odio variualiqua. Curabitur vestibulum, quam et dignissim porttitor, diam erat varius est, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue."
-
+      keywordsCard: 'Fusce nec tempus dolor, eu egestas magna. eu egestas magna. Fuscenec tempus dolor, eu egestas magna. eu egestas magna.'
+      date: '10.12.2019'
     };
   }
 
   render() {
     return(
-        <CardComponent headerColor={this.state.headerColor} chapterCard={this.state.chapterCard}/>
+        <CardComponent 
+          headerColor={this.state.headerColor} 
+          chapterCard={this.state.chapterCard} 
+          textCard={this.state.textCard}
+          keywordsCard={this.state.keywordsCard}
+          date={this.state.date}
+        />
     );
   }
 }

7aef576fc65f2c4e27730444faad26f535629fb8
Antonia Dobrescu <antonia.dobrescu91@gmail.com>
2019-12-12 14:53:50 +0200

    changed the width and height

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import CardComponent from '../src/components/CardComponent/CardComponent'
+import React from "react";
+import CardComponent from "../src/components/CardComponent/CardComponent";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       headerColor: "#FA5457",
-      chapterCard: 'Chapter Title',
-      textCard: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue eros egestas aliquet viverra. Mauris feugiat ultrices odi variualiqua. Curabitur vestibulum, quam et dignissim porttitor,diam erat varius est Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue eros egestas aliquet viverra. Mauris feugiat ultrices odio variualiqua. Curabitur vestibulum, quam et dignissim porttitor, diam erat varius est, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis congue.",
-      keywordsCard: 'Fusce nec tempus dolor, eu egestas magna. eu egestas magna. Fuscenec tempus dolor, eu egestas magna. eu egestas magna.',
-      date: '10.12.2019'
+      chapterCard: "Chapter Title",
+      textCard:
+        "Lorem ipsum dolor sit aliquet viverra. estibulum, quam et dignissim porttitor,diam erat varius est Lorem  Duis congue eros egestas aliquet viverra. Mauris feugiat ultrices odio variualiqua. Curabitur vestibulum, quam et dignissim porttitor, diam erat varius est, Lorem ipsum dolor sit a. Duis congue.",
+      keywordsCard:
+        "Fusce nec tempus dolor, eu egestas magna. eu egestas magna. Fuscenec tempus dolor, eu egestas magna. eu egestas magna.",
+      date: "10.12.2019"
     };
   }
 
   render() {
-    return(
-        <CardComponent 
-          headerColor={this.state.headerColor} 
-          chapterCard={this.state.chapterCard} 
+    return (
+      <div>
+        <CardComponent
+          headerColor={this.state.headerColor}
+          chapterCard={this.state.chapterCard}
           textCard={this.state.textCard}
           keywordsCard={this.state.keywordsCard}
           date={this.state.date}
         />
+        <CardComponent
+          headerColor={this.state.headerColor}
+          chapterCard={this.state.chapterCard}
+          textCard={this.state.textCard}
+          keywordsCard={this.state.keywordsCard}
+          date={this.state.date}
+        />
+        <CardComponent
+          headerColor={this.state.headerColor}
+          chapterCard={this.state.chapterCard}
+          textCard={this.state.textCard}
+          keywordsCard={this.state.keywordsCard}
+          date={this.state.date}
+        />
+      </div>
     );
   }
 }

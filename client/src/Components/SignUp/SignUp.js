24393ca604cc9cffc291c9c989e8ce82dcf93445
VSPaul <vascupaul_vsp@yahoo.com>
2020-02-03 17:57:22 +0200

    fixdate

--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -10,73 +10,73 @@ import DateFnsUtils from '@date-io/date-fns';
 import logo from '../../assets/images/logo.png';
 import './SignUp.css';
 
-class SignUp extends React.Component{
-    constructor(props){
+class SignUp extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
-                    nume : '',
-                    prenume: '',
-                    email: '',
-                    numarTelefon : '',
-                    judetul: '',
-                    centrul: '',
-                    nrcontractului: '',
-                    dataSemnarii: '',
-                    parola: '',
-                    parolaConfirmed: false,
-                    id: 0,
-                    flash:''
-                }
+            nume: '',
+            prenume: '',
+            email: '',
+            numarTelefon: '',
+            judetul: '',
+            centrul: '',
+            nrcontractului: '',
+            dataSemnarii: '',
+            parola: '',
+            parolaConfirmed: false,
+            id: 0,
+            flash: ''
+        }
     }
 
     handleChange = input => e => {
         this.setState({
             [input]: e.target.value
         })
-    console.log(this.state)
+        console.log(this.state)
     }
 
     handlePasswordConfirm = (e) => {
-        this.state.parola === e.target.value 
-        ? this.setState({ parolaConfirmed : true }) 
-        : this.setState({ parolaConfirmed :false})
+        this.state.parola === e.target.value
+            ? this.setState({ parolaConfirmed: true })
+            : this.setState({ parolaConfirmed: false })
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
         fetch('auth/signup',
-        {
-            method : 'POST',
-            headers: new Headers({
-                'Content-Type' : 'application/json'
-            }),
-            body: JSON.stringify(this.state)
-        })
-        .then(res => res.json())
-        .then(res => {
-        console.log(res.message)
-        console.log(this.props.history)
-            this.props.history.replace('/')
-        })
-        .catch(err => this.setState({
-            flash:err.message
-        }))
+            {
+                method: 'POST',
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                }),
+                body: JSON.stringify(this.state)
+            })
+            .then(res => res.json())
+            .then(res => {
+                console.log(res.message)
+                console.log(this.props.history)
+                this.props.history.replace('/')
+            })
+            .catch(err => this.setState({
+                flash: err.message
+            }))
     }
 
     handleDateChange = date => {
         this.setState({
-            dataSemnarii : date
+            dataSemnarii: date
         })
     };
 
-    render(){
+    render() {
         console.log(this.state.dataSemnarii)
-        return(
+        return (
             <div className="main">
                 <header>
                     <div className="container-fluid headerMain">
                         <div className="logoPlaceholder">
-                            <img src={logo} alt="logo" className="logo"/>
+                            <img src={logo} alt="logo" className="logo" />
                         </div>
                     </div>
                 </header>
@@ -86,86 +86,87 @@ class SignUp extends React.Component{
                 <form className="formMain" autoComplete="off">
                     <div className="topForm">
                         <div className="row">
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Nume"
-                                    className="signUpInput signUpInputLeft"
-                                    margin="normal"
-                                    variant="filled"
-                                    value={this.state.nume}
-                                    onChange={this.handleChange('nume')}
-                                />
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Prenume"
-                                    className="signUpInput signUpInputRight"
-                                    margin="normal"
-                                    variant="filled"
-                                    value={this.state.prenume}
-                                    onChange={this.handleChange('prenume')}
-                                />
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Nume"
+                                className="signUpInput signUpInputLeft"
+                                margin="normal"
+                                variant="filled"
+                                value={this.state.nume}
+                                onChange={this.handleChange('nume')}
+                            />
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Prenume"
+                                className="signUpInput signUpInputRight"
+                                margin="normal"
+                                variant="filled"
+                                value={this.state.prenume}
+                                onChange={this.handleChange('prenume')}
+                            />
                         </div>
 
                         <div className="row">
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Email"
-                                    type="email"
-                                    className="signUpInput signUpInputLeft"
-                                    margin="normal"
-                                    variant="filled"
-                                    value={this.state.email}
-                                    onChange={this.handleChange('email')}
-                                    />
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Numar Telefon"
-                                    className="signUpInput signUpInputRight"
-                                    margin="normal"
-                                    variant="filled"
-                                    value={this.state.numarTelefon}
-                                    onChange={this.handleChange('numarTelefon')}
-                                />
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Email"
+                                type="email"
+                                className="signUpInput signUpInputLeft"
+                                margin="normal"
+                                variant="filled"
+                                value={this.state.email}
+                                onChange={this.handleChange('email')}
+                            />
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Numar Telefon"
+                                className="signUpInput signUpInputRight"
+                                margin="normal"
+                                variant="filled"
+                                value={this.state.numarTelefon}
+                                onChange={this.handleChange('numarTelefon')}
+                            />
                         </div>
 
                         <div className="row">
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Judetul in care esti voluntar"
-                                    className="signUpInput signUpInputLeft"
-                                    margin="normal"
-                                    variant="filled"
-                                    value={this.state.judetul}
-                                    onChange={this.handleChange('judetul')}
-                                />
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Centrul in care esti voluntar"
-                                    className="signUpInput signUpInputRight"
-                                    margin="normal"
-                                    variant="filled"
-                                    value={this.state.centrul}
-                                    onChange={this.handleChange('centrul')}
-                                />
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Judetul in care esti voluntar"
+                                className="signUpInput signUpInputLeft"
+                                margin="normal"
+                                variant="filled"
+                                value={this.state.judetul}
+                                onChange={this.handleChange('judetul')}
+                            />
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Centrul in care esti voluntar"
+                                className="signUpInput signUpInputRight"
+                                margin="normal"
+                                variant="filled"
+                                value={this.state.centrul}
+                                onChange={this.handleChange('centrul')}
+                            />
                         </div>
 
                         <div className="row">
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Numarul contractului de voluntar"
-                                    className="signUpInput signUpInputLeft"
-                                    margin="normal"
-                                    variant="filled"
-                                    value={this.state.nrcontractului}
-                                    onChange={this.handleChange('nrcontractului')}                                    />
-                                <TextField
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Numarul contractului de voluntar"
+                                className="signUpInput signUpInputLeft"
+                                margin="normal"
+                                variant="filled"
+                                value={this.state.nrcontractului}
+                                onChange={this.handleChange('nrcontractului')}
+                            />
+                            {/* <TextField
                                     required
                                     id="filled-required"
                                     label="Data semnarii contractului de voluntar (ex: 2020-01-23)"
@@ -174,7 +175,22 @@ class SignUp extends React.Component{
                                     variant="filled"
                                     value={this.state.dataSemnarii}
                                     onChange={this.handleChange('dataSemnarii')}
-                                />
+                                /> */}
+
+                            <TextField
+                                require
+                                id="date"
+                                label="Data semnarii contractului de voluntar (ex: 10-23-2020)"
+                                type="date"
+                                // defaultValue="2017-05-24"
+                                className="signUpInput signUpInputRight"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                value={this.state.dataSemnarii}
+                                onChange={this.handleChange('dataSemnarii')}
+                            />
+
 
                             {/* <MuiPickersUtilsProvider utils={DateFnsUtils}>
                                     <KeyboardDatePicker
@@ -197,35 +213,35 @@ class SignUp extends React.Component{
                     </div>
                     <div className="bottomForm">
                         <div className="row">
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Parola"
-                                    type="password"
-                                    className="signUpInput"
-                                    margin="normal"
-                                    variant="filled"
-                                    value={this.state.parola}
-                                    onChange={this.handleChange('parola')}
-                                />
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Parola"
+                                type="password"
+                                className="signUpInput"
+                                margin="normal"
+                                variant="filled"
+                                value={this.state.parola}
+                                onChange={this.handleChange('parola')}
+                            />
                         </div>
 
                         <div className="row">
-                                <TextField
-                                    required
-                                    id="filled-required"
-                                    label="Confirmare parola"
-                                    type="password"
-                                    className="signUpInput"
-                                    margin="normal"
-                                    variant="filled"
-                                    onChange={this.handlePasswordConfirm}
-                                />
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Confirmare parola"
+                                type="password"
+                                className="signUpInput"
+                                margin="normal"
+                                variant="filled"
+                                onChange={this.handlePasswordConfirm}
+                            />
                         </div>
                     </div>
                     <div className="signUpBtnContainer">
-                            <button label="Submit" type="submit" onClick={this.handleSubmit}>
-                                Inscriere
+                        <button label="Submit" type="submit" onClick={this.handleSubmit}>
+                            Inscriere
                             </button>
                     </div>
                 </form>

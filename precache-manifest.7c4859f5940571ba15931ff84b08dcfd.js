c7b6637995904382a7b6bf0b8ea3602792d67746
Olivier Ggr <grandgerard.olivier@gmail.com>
2020-02-04 17:52:21 +0200

    Updates

---
+++ precache-manifest.7c4859f5940571ba15931ff84b08dcfd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e092bfbbbdd04e14cd7022c82b442fb",
+    "url": "/bucharest-project3-online-library/index.html"
+  },
+  {
+    "revision": "9c7dbc662bf7084b8c53",
+    "url": "/bucharest-project3-online-library/static/css/main.b485a339.chunk.css"
+  },
+  {
+    "revision": "b84e8f9ba2ad6add6b0d",
+    "url": "/bucharest-project3-online-library/static/js/2.e6b25b8a.chunk.js"
+  },
+  {
+    "revision": "f8aaca4be97bafae96fa414e7664b8da",
+    "url": "/bucharest-project3-online-library/static/js/2.e6b25b8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c7dbc662bf7084b8c53",
+    "url": "/bucharest-project3-online-library/static/js/main.ad3100ab.chunk.js"
+  },
+  {
+    "revision": "622e2a111caa8f2e87ed",
+    "url": "/bucharest-project3-online-library/static/js/runtime-main.200e897a.js"
+  },
+  {
+    "revision": "db5228b405d6f9eb54176b01ffc44d80",
+    "url": "/bucharest-project3-online-library/static/media/children.db5228b4.png"
+  },
+  {
+    "revision": "16dda1fa7aaccb9e57cde03a0e14c62d",
+    "url": "/bucharest-project3-online-library/static/media/logo.16dda1fa.png"
+  },
+  {
+    "revision": "ba048dd19a51b4ed58050095b226fa5d",
+    "url": "/bucharest-project3-online-library/static/media/trophy.ba048dd1.png"
+  }
+]);
